09618d1a43f5dfe0ebd363b0dfb19f316a890ac6
enggar tivandi <enggartivandi@outlook.com>
2021-03-06 17:47:08 +0800

    update

--- stikom-jsi.js
+++ stikom-jsi.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 const $ = require("cheerio");
 const fs = require("fs");
+const logger = fs.createWriteStream("jsi-stikom.txt", {
+  flags: "a+",
+});
 
 const getAllIssues = async () => {
   const base_url = "https://jsi.stikom-bali.ac.id/index.php/jsi/issue/archive";
@@ -23,18 +26,36 @@ const getIssue = async (url) => {
 };
 
 const parserHtml = async (selector, data) => {
-  const result = await $(selector, data);
+  const result = $(selector, data);
   return result;
+};
+
+const getJournal = async (journal) => {
+  const journalTitle = $("h3 > a", journal)
+    .children()[0]
+    .next.data.replaceAll("\t", "");
+
+  const journalLink = $("div.btn-group > a", journal)[0]?.attribs.href;
+
+  return [journalTitle, journalLink];
 };
 
 getAllIssues().then(async (response) => {
   const issues = await parserHtml(".issues.media-list > * a.cover", response);
 
   for (const issue of issues) {
-    const linkIssue = await getIssue(issue.attribs.href);
-    const journals = await parserHtml(
-      ".article-summary.media > div.media-body",
-      linkIssue
-    );
+    const issueLink = await getIssue(issue.attribs.href);
+    const journals = $("div.media-list > div.article-summary", issueLink);
+
+    for (const journal of journals) {
+      const result = await getJournal(journal);
+      logger.write(
+        `\nTitle : ${result[0].replaceAll("\n", "")}\nLink : ${result[1]}`
+      );
+    }
   }
+
+  logger.end();
+
+  console.info("done");
 });
